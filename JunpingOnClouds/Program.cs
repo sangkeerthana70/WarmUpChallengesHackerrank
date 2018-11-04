3b0fc046898c4668410c6c8a3d90a5d6b2f1780f
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-03 20:45:59 -0400

    Jumping On Clouds Hackerrank warm up challenge

--- JunpingOnClouds/Program.cs
+++ JunpingOnClouds/Program.cs
@@ -7,6 +7,94 @@ namespace JunpingOnClouds
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] cloudArray = new int[] { 0, 0, 1, 0, 0, 1, 0 };
+            FindNumberOfCloudsJumped(cloudArray);
+
         }
+
+
+        static int FindNumberOfCloudsJumped(int[] cloudArray)
+        {
+
+            var targetIndex = 0;
+            var jumpCount = 0;
+            for (int i = 0; i < cloudArray.Length; i++)
+            {
+                if (cloudArray[i] == 1)
+                {
+                    continue;
+                }
+
+                //if (cloudArray[i] == 0)
+                //{
+                //    Console.WriteLine("Cumulus");
+                //}
+                //else
+                //    Console.WriteLine("Thunderhead");
+
+                //Console.WriteLine("Loop count is: " + i);
+                Console.WriteLine("Current Index is: " + i);
+                //Console.WriteLine("Value of this index is: " + cloudArray[i]);
+
+                
+
+                
+
+                //if (i < cloudArray.Length -1)
+                //{
+                //    //Console.WriteLine("Next Index is: " + (i + 1));
+                //    //Console.WriteLine("Value of next index is: " + cloudArray[i + 1]);
+
+                    
+                //}
+
+                if (i == cloudArray.Length - 2)
+                {
+                    //Console.WriteLine("Jump 1 cloud");
+                    targetIndex = i + 1;
+                    Console.WriteLine("Target Index: " + targetIndex);
+                    jumpCount += 1;
+                }
+
+
+
+                if (i < cloudArray.Length - 2)
+                {
+                    //Console.WriteLine("Next to Next Index is: " + (i + 2));
+                    var NextNextValueinArr = cloudArray[i + 2];
+                    //Console.WriteLine("Next to next value is: " + cloudArray[i + 2]);
+
+                    if (NextNextValueinArr == 0)
+                    {
+
+                        //Console.WriteLine("Jump 2 clouds");
+                        targetIndex = i + 2;
+                        Console.WriteLine("Target Index: " + targetIndex);
+                        jumpCount += 1;
+                        i += 1;
+                    }
+                    else
+                    {
+                        //Console.WriteLine("Jump 1 cloud");
+                        targetIndex = i + 1;
+                        Console.WriteLine("Target Index: " + targetIndex);
+                        jumpCount += 1;
+                    }
+
+
+                }
+
+
+                Console.WriteLine("Jump Count: " + jumpCount);
+                Console.WriteLine();
+
+               
+            }
+            Console.WriteLine("Jump Count: " + jumpCount);
+
+
+            return jumpCount;
+        }
+
     }
 }
